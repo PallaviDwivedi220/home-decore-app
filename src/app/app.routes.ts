f0c01e11b9ae56772f935d400b8d1e8a01c6bdee
PallaviDwivedi220 <pallavi.dwivedi220@gmail.com>
2025-04-13 10:58:45 +0530

    mobile design improvements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,4 @@
 import { Routes } from '@angular/router';
-import { LivingRoomComponent } from './living-room/living-room.component';
-import { SalePageComponent } from './sale-page/sale-page.component';
-import { BedroomPageComponent } from './bedroom-page/bedroom-page.component';
-import { BathroomPageComponent } from './bathroom-page/bathroom-page.component';
-import { KitchenPageComponent } from './kitchen-page/kitchen-page.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
